a696de99aa572590615b8ccf4869e14e8559f468
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-07-26 12:47:45 -0400

    route order and preparing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,10 @@ module.exports = {
       'green':'#B9C48B',
       'green-light':'#F4FDD0',
       'green-dark':'#414531',
+    },
+    boxShadow:{
+      'full': '0 5px 5px rgba(0,0,0,0.5)',
+      '3xl': '0 5px 5px rgba(0,0,0,0.3)'
     }
   },
   plugins: [],
