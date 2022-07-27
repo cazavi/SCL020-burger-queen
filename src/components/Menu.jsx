5baea41af7e4e3d28f52e064a0614e114846c182
Carolina <carolinazapatavidal@gmail.com>
2022-07-27 11:34:31 -0600

    menu with order interaction

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Menu({menu}){
+
+  return (
+    <div className="font-Comfortaa grid grid-cols-2 gap-8 my-8">
+      {menu.map(item => (
+        <button key={item.id} className="bg-[#F9D67E] rounded-full h-28 w-56 shadow-full">
+          <h2>{item.name}</h2>
+          <h3 className="hidden">{item.cost}</h3>
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
