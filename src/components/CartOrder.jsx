8d6ac0a336b4b5bbe94d05a21350c52536ed5c3d
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-08-04 11:23:55 -0400

    send order

--- src/components/CartOrder.jsx
+++ src/components/CartOrder.jsx
@@ -6,6 +6,7 @@ function CartOrder({ menu }) {
     const context = useContext(AppContext);
     const cartProduct = context.cart;
     const deleteButton = context.deleteItem;
+    
 
     const totalPrice = () => {
         let PriceTotal = 0;
@@ -16,23 +17,25 @@ function CartOrder({ menu }) {
     }
     const totalPruduct = () => {
         return cartProduct.reduce((previous, current) => previous + current.qty, 0)
-         
     }
 
     return (
         <>
-            {cartProduct.map((item, index) => (
-                <div key={index} className="flex w-56 mt-1.5 ">
-                    <button> <RiDeleteBin2Fill className="ml-2" color="#E6553C" size={40} onClick={() => deleteButton(item)} /></button>
-                    <p className="w-14 ml-4 flex ">{item.product}</p>
-                    <p className="w-10 ml-20 flex ">{item.qty}</p>
-                    <p className="w-10 ml-20 flex font-extrabold">{item.totalPrice}</p>
-                </div>
-            ))}
-            <div className="flex items-end justify-around mt-[auto]">
+            <div className="h-[32rem] overflow-y-auto">
+                {cartProduct.map((item, index) => (
+                    <div key={index} className="flex w-56 mt-1.5 ">
+                        <button> <RiDeleteBin2Fill className="ml-2" color="#E6553C" size={40} onClick={() => deleteButton(item)} /></button>
+                        <p className="w-14 ml-4 flex ">{item.product}</p>
+                        <p className="w-10 ml-20 flex ">{item.qty}</p>
+                        <p className="w-10 ml-20 flex font-extrabold">{item.totalPrice}</p>
+                    </div>
+                ))}
+            </div>
+            <hr />
+            <div className="flex items-end justify-around mt-[auto] mb-2">
                 <p className="">Total:</p>
                 <p className="">{totalPruduct()}</p>
-                <p className="">${totalPrice()}</p>
+                <p className={localStorage.role == "mesero"? "flex":"invisible"}>${totalPrice()}</p>
             </div>
         </>
     )
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -14,51 +14,55 @@ const Login = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		axios
-		.post("https://apiburgerqueenv1.herokuapp.com/auth", {
-			email: user.email, 
-			password: user.password, 
-		})
-		.then((response) => {
-			console.log(response.data);
-			localStorage.setItem("token", response.data.token);
-			localStorage.setItem("role", response.data.data.role.description);
-			localStorage.setItem("email", response.data.data.email);
-			response = navigate("/options"); 
-		})
+			.post("https://apiburgerqueenv1.herokuapp.com/auth", {
+				email: user.email,
+				password: user.password,
+			})
+			.then((response) => {
+				console.log(response.data);
+				localStorage.setItem("token", response.data.token);
+				localStorage.setItem("role", response.data.data.role.description);
+				localStorage.setItem("email", response.data.data.email);
+				if(response.data.data.role.description == 'admin'){
+					response = navigate("/options");
+				}else{
+					response = navigate("/tables");
+				}
+			})
 
-		.catch((err) => console.log(err));
+			.catch((err) => console.log(err));
 	};
-	return(
-			<div className="bg-[url('./IMGS/BACKGROUND.jpg')] bg-cover w-full h-[820px] flex items-center flex-col font-Comfortaa">
-					<picture className="my-16">
-							<img alt='Logo' src='./IMGS/nonna queen.png'/>
-					</picture>
-					<div className='bg-green backdrop-blur-xl bg-opacity-70 max-w-md drop-shadow-lg rounded-xl px-10 pt-10 pb-10 text-center inline-block'>
-							<form 
-									onSubmit={handleSubmit}
-							>
-									<label htmlFor="email">Usuario</label>
-									<input 
-											className='bg-green-light rounded text-center block border-green-dark'
-											type='email'
-											name='email'
-											id='email'
-											placeholder='usuario'
-											onChange={handleChange}
-									></input>
-									<label htmlFor="password">Contraseña</label>
-									<input 
-											className='bg-green-light rounded text-center block border-green-dark'
-											id='password'
-											type='password'
-											name='password'
-											placeholder='contraseña'
-											onChange={handleChange}
-											></input>
-									<button className='bg-[#529A6B]  ml-auto mr-auto rounded-full block shadow-md mt-4 w-40 h-10 text-[#ffffff] text-bold border-green-dark border-2' type='submit'>Acceder</button>
-							</form>
-					</div>
+	return (
+		<div className="bg-[url('./IMGS/BACKGROUND.jpg')] bg-cover w-full h-[820px] flex items-center flex-col font-Comfortaa">
+			<picture className="my-16">
+				<img alt='Logo' src='./IMGS/nonna queen.png' />
+			</picture>
+			<div className='bg-green backdrop-blur-xl bg-opacity-70 max-w-md drop-shadow-lg rounded-xl px-10 pt-10 pb-10 text-center inline-block'>
+				<form
+					onSubmit={handleSubmit}
+				>
+					<label htmlFor="email">Usuario</label>
+					<input
+						className='bg-green-light rounded text-center block border-green-dark'
+						type='email'
+						name='email'
+						id='email'
+						placeholder='usuario'
+						onChange={handleChange}
+					></input>
+					<label htmlFor="password">Contraseña</label>
+					<input
+						className='bg-green-light rounded text-center block border-green-dark'
+						id='password'
+						type='password'
+						name='password'
+						placeholder='contraseña'
+						onChange={handleChange}
+					></input>
+					<button className='bg-[#529A6B]  ml-auto mr-auto rounded-full block shadow-md mt-4 w-40 h-10 text-[#ffffff] text-bold border-green-dark border-2' type='submit'>Acceder</button>
+				</form>
 			</div>
+		</div>
 	);
 }
 
