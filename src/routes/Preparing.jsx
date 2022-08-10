703984b9ccbfc3441f7411ac2ac21302ed2c71d6
Carolina <carolinazapatavidal@gmail.com>
2022-08-10 12:41:41 -0400

    showing order to chef

--- src/routes/Preparing.jsx
+++ src/routes/Preparing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 const Preparing = () => {
 
@@ -9,34 +10,25 @@ const Preparing = () => {
     const [order, setOrder] = useState();
     const [table, setTable] = useState(state)
     console.log(menu)
-    console.log(order.products)
+    console.log(order)
     console.log(table)
 
     useEffect(() => {
-        getOrder()
+        handleGetOrder()
     }, [])
 
     useEffect(() => {
         filterMenu(table)
     }, [menu])
 
-    const getOrder = () => {
-        let headers = {
-            Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-        fetch('https://apiburgerqueenv1.herokuapp.com/orders', { headers })
-            .then((response) => response.json())
-            .then((data) => {
-                setMenu(data.result);
-            });
-    }
     const filterMenu = (table) => {
         const result = menu.filter(item => item.table === table && item.status !== null);
         setOrder(result[0]);
     };
-    const totalProduct = () => {
-        return order.reduce((previous, current) => previous + current.qty, 0)
 
+    const totalProduct = () => {
+        return order.products.reduce((previous, current) => previous + current.qty, 0)
+    }
 
     const handleGetOrder = () => {
         let headers = {
@@ -45,10 +37,10 @@ const Preparing = () => {
         axios
         .get('https://apiburgerqueenv1.herokuapp.com/orders', { headers })
         .then((response) => {
-            console.log(response);
+            console.log(response.data)
+            setMenu(response.data.result);
         })
-        .catch((err) => console.log(err.response.data));;
-
+        .catch((err) => console.log(err.response));;
     }
 
     return (
@@ -58,12 +50,13 @@ const Preparing = () => {
                 <div className="flex flex-col items-center">
                     <div className="bg-green w-96 h-5/6 flex rounded-md shadow-3xl">
                         <div className="h-[32rem] overflow-y-auto">
-                            {order.map((item, index) => (
+                            {order.products.map((item, index) => (
                                 <div key={index} className="flex w-56 mt-1.5 ">
                                     <p className="w-14 ml-4 flex ">{item.product}</p>
                                     <p className="w-10 ml-20 flex ">{item.qty}</p>
                                 </div>
                             ))}
+                            
                         </div>
                         <hr />
                         <div className="flex items-end justify-around mt-[auto] mb-2">
@@ -72,9 +65,7 @@ const Preparing = () => {
                         </div>
                     </div>
                     <div className="bg-[#B6CE55] flex my-4 h-16 w-5/6 items-end rounded-md">
-                        {order.map((item) => (
-                            <h1 className="mt-4 mb-4 ml-4 bg-[#B6CE55]">Cliente: {item.client}</h1>
-                            ))}
+                            <h1 className="mt-4 mb-4 ml-4 bg-[#B6CE55]">Cliente: {order.client}</h1>
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-evenly">
